ffac9ce75c3ffc0c7a5721d6e96d52b1326f32aa
hebamohameedd <heba29715@gmail.com>
2026-02-28 00:19:00 +0200

    Update

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,56 +1,22 @@
-// import {Route, Routes} from "react-router-dom";
-// import Login from "./pages/Login.jsx";
-// import Signup from "./pages/Signup.jsx";
-// import DashBoard from "./pages/DashBoard.jsx";
-// // import toast from "react-hot-toast";
-//
-//
-// const App = () => {
-//     return (<div data-theme="forest">
-//             {/*<button onClick={() => toast.success("congrats")} className="btn btn-primary"> Click me</button>*/}
-//             {/*<button className="btn btn-primary">Register</button>*/}
-//             {/*<button className="btn btn-secondary">Two</button>*/}
-//             {/*<button className="btn btn-accent btn-outline">Three</button>*/}
-//             <Routes>
-//                 <Route path="/" element={<Login/>}/>
-//                 <Route path="/register" element={<Signup/>}/>
-//                 <Route path="/admin" element={<DashBoard/>}/>
-//             </Routes>
-//
-//
-//         </div>
-//     );
-// };
-//
-// export default App;
-
-
-
-
-
-
-
-
-// import {useState} from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Signup from './pages/Signup.jsx'
-import {Routes, Route} from "react-router-dom";
-import Login from "./pages/Login.jsx";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar'; 
+import HomePage from './pages/HomePage'; 
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
-    // const [count, setCount] = useState(0)
-
     return (
-        <div>
-            {/*<BrowserRouter>*/}
-                <Routes>
-                    <Route path='/register' element={<Signup/>}/><Route/>
-                    <Route path='/login' element={<Login/>}/><Route/>
-                </Routes>
-            {/*</BrowserRouter>*/}
-        </div>
+        <>
+            {}
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path='/register' element={<Signup />} />
+                <Route path='/login' element={<Login />} />
+            </Routes>
+        </>
     )
 }
 
-export default App
+export default App;
